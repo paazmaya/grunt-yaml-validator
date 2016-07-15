e5cd9caff22e8212a12384c8c90e89f5605c0cf8
Juga Paazmaya <paazmaya@yahoo.com>
2016-07-15 13:59:23 +0300

    Secure every :pig:

--- test/yaml_validator_test.js
+++ test/yaml_validator_test.js
@@ -2,7 +2,7 @@
  * grunt-yaml-validator
  * https://github.com/paazmaya/grunt-yaml-validator
  *
- * Copyright (c) Juga Paazmaya <paazmaya@yahoo.com> (http://paazmaya.fi)
+ * Copyright (c) Juga Paazmaya <paazmaya@yahoo.com> (https://paazmaya.fi)
  * Licensed under the MIT license.
  */
 
